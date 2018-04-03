50c7d4d737b799955f2ed522346311198df4d68c
Sunny <sundeepkmanik@gmail.com>
2018-04-03 00:00:27 -0700

    doc ready function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,80 +1,84 @@
-// ========== global variables ==========
-// ======================================
+$(document).ready(function() {
 
-// our array of classic movies
-var topicsArr = ["the big sleep", "vertigo", "casablanca", "citizen kane", "an american in paris", "psycho", "rear window", "a streetcar named desire", "gone with the wind", "miracle on 34th street", "duck soup", "seven samurai", "rashomon", "the maltese falcon"];
+    // ========== global variables ==========
+    // ======================================
 
-// ============== actions ===============
-// ======================================
+    // our array of classic movies
+    var topicsArr = ["the big sleep", "vertigo", "casablanca", "citizen kane", "an american in paris", "psycho", "rear window", "a streetcar named desire", "gone with the wind", "miracle on 34th street", "duck soup", "seven samurai", "rashomon", "the maltese falcon"];
 
-// function that updates tag display
-function updateButtons() {
+    // ============== actions ===============
+    // ======================================
 
-    $("#tag-content").empty();
+    // function that updates tag display
+    function updateButtons() {
 
-    for (var i = 0; i < topicsArr.length; i++) {
-    
-        var tagButton = $("<button>");
-        tagButton.attr("data-movie", topicsArr[i]);
-        tagButton.text(topicsArr[i]);
+        $("#tag-content").empty();
 
-        $("#tag-content").append(tagButton);
+        for (var i = 0; i < topicsArr.length; i++) {
+        
+            var tagButton = $("<button>");
+            tagButton.attr("data-movie", topicsArr[i]);
+            tagButton.text(topicsArr[i]);
 
-    }
-
-};
-
-// function that lets user add to array and updates tag display
-$("#add-button").on("click", function () {
-
-    event.preventDefault();
-
-    var textInput = $("#addbutton-input").val().trim();
-
-    topicsArr.push(textInput);
-    console.log(topicsArr);
-    
-    // display
-    updateButtons();
-
-});
-
-// function that presents giphy gifs when user clicks tags 
-$(document.body).on("click", "button", function () {
-
-    // display
-    $("#gifs-content").empty();
-
-    var movie = $(this).attr("data-movie");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-
-        console.log(response);
-        var results = response.data;
-
-        for (var i = 0; i < results.length; i++) {
-
-            var movieDiv = $("<div>");
-            
-            var p = $("<p>");
-            p.text(results[i].rating);
-
-            var movieImage = $("<img>");
-            movieImage.attr("src", results[i].images.fixed_height.url);
-
-            // display
-            movieDiv.append(p);
-            movieDiv.append(movieImage);
-            $("#gifs-content").append(movieDiv);
+            $("#tag-content").append(tagButton);
 
         }
 
-    });
-});
+    };
 
-// presents initial tag display when page first loads
-updateButtons();
+    // function that lets user add to array and updates tag display
+    $("#add-button").on("click", function () {
+
+        event.preventDefault();
+
+        var textInput = $("#addbutton-input").val().trim();
+
+        topicsArr.push(textInput);
+        console.log(topicsArr);
+        
+        // display
+        updateButtons();
+
+    });
+
+    // function that presents giphy gifs when user clicks tags 
+    $(document.body).on("click", "button", function () {
+
+        // display
+        $("#gifs-content").empty();
+
+        var movie = $(this).attr("data-movie");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            console.log(response);
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+
+                var movieDiv = $("<div>");
+                
+                var p = $("<p>");
+                p.text(results[i].rating);
+
+                var movieImage = $("<img>");
+                movieImage.attr("src", results[i].images.fixed_height.url);
+
+                // display
+                movieDiv.append(p);
+                movieDiv.append(movieImage);
+                $("#gifs-content").append(movieDiv);
+
+            }
+
+        });
+    });
+
+    // presents initial tag display when page first loads
+    updateButtons();
+
+});
